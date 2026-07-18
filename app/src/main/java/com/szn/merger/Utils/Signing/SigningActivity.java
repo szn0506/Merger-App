b4e546bddc201d98176cdb7d61e29691383a1153
szn0506 <291541985+szn0506@users.noreply.github.com>
2026-07-19 01:32:28 +0800

    fix state button bug

--- app/src/main/java/com/szn/merger/Utils/Signing/SigningActivity.java
+++ app/src/main/java/com/szn/merger/Utils/Signing/SigningActivity.java
@@ -3,10 +3,12 @@ package com.szn.merger.Utils.Signing;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.checkbox.MaterialCheckBox;
 import com.szn.merger.CustomSwitchItem;
@@ -17,6 +19,7 @@ public class SigningActivity extends AppCompatActivity {
     private CustomSwitchItem signSwitch;
     private MaterialCardView signSchemes;
     private static MaterialCheckBox V1, V2, V3, V4;
+    private TextView currentSchemes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +29,14 @@ public class SigningActivity extends AppCompatActivity {
         setContentView(R.layout.signing_layout);
         initView();
         setupListener();
+        loadState();
+        updatePlaceholder();
     }
 
     private void initView() {
         signSwitch = findViewById(R.id.SignSwitch);
         signSchemes = findViewById(R.id.signSchemes);
+        currentSchemes = findViewById(R.id.currentSchemes);
     }
     private void setupListener() {
         signSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
@@ -39,6 +45,7 @@ public class SigningActivity extends AppCompatActivity {
         signSchemes.setOnClickListener(v -> {
             showBottomSheet();
         });
+
     }
     private void showBottomSheet() {
         BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
@@ -50,7 +57,7 @@ public class SigningActivity extends AppCompatActivity {
         V2 = bottomSheetView.findViewById(R.id.checkV2);
         V3 = bottomSheetView.findViewById(R.id.checkV3);
         V4 = bottomSheetView.findViewById(R.id.checkV4);
-
+        MaterialButton doneButton = bottomSheetView.findViewById(R.id.doneButton);
         restoreState();
 
         V1.setOnCheckedChangeListener((buttonView, isChecked) ->
@@ -64,11 +71,37 @@ public class SigningActivity extends AppCompatActivity {
 
         V4.setOnCheckedChangeListener((buttonView, isChecked) ->
                 SigningManager.setV4Enabled(this, isChecked));
+        doneButton.setOnClickListener(v -> {
+            updatePlaceholder();
+            bottomSheetDialog.dismiss();
+        });
     }
     private void restoreState() {
         V1.setChecked(SigningManager.isV1Enabled(this));
         V2.setChecked(SigningManager.isV2Enabled(this));
         V3.setChecked(SigningManager.isV3Enabled(this));
         V4.setChecked(SigningManager.isV4Enabled(this));
+    }
+    private void updatePlaceholder() {
+        StringBuilder schemes = new StringBuilder();
+
+        if (SigningManager.isV1Enabled(this)) schemes.append("V1");
+        if (SigningManager.isV2Enabled(this)) {
+            if (schemes.length() > 0) schemes.append(", ");
+            schemes.append("V2");
+        }
+        if (SigningManager.isV3Enabled(this)) {
+            if (schemes.length() > 0) schemes.append(", ");
+            schemes.append("V3");
+        }
+        if (SigningManager.isV4Enabled(this)) {
+            if (schemes.length() > 0) schemes.append(", ");
+            schemes.append("V4");
+        }
+
+        currentSchemes.setText(schemes.length() > 0 ? schemes.toString() : "None");
+    }
+    private void loadState() {
+        signSwitch.setChecked(SigningManager.isSignEnabled(this));
     }
 }
